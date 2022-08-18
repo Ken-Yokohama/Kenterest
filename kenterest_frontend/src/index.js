7c9d9a7ada4273375bad6bbe5e0d6219a483b56b
Ken Yokohama <kenlopezyokohama@gmail.com>
2022-08-18 08:20:44 +0800

    Removed new boilerplate for React Router

--- kenterest_frontend/src/index.js
+++ kenterest_frontend/src/index.js
@@ -10,30 +10,3 @@ ReactDOM.render(
     </Router>,
     document.getElementById('root')
 );
-
-//**New BoilerPLate for Router
-//**Place this inside index.js and wrap <Router> around <App />
-// import { BrowserRouter as Router } from 'react-router-dom';
-// ReactDOM.render(
-//     <Router>
-//         <App />
-//     </Router>,
-//     document.getElementById('root')
-// );
-//
-// **Place this inside App .js
-//
-// import {Routes, Route, useNavigate} from 'react-router-dom';
-// **Note: Switch was Renamed Routes in latest Update
-//
-// function App(){
-//     return (
-//         <Routes>
-//             <Route path="login" element={<Login />} />
-//             <Route path="/*" element={<Home />} />
-//**Note: Make a Functional Component for <Login /> & <Home />
-//         </Routes>
-//     )
-// }
-//
-// export default App;
